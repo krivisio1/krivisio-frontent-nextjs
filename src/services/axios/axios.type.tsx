ad380f8872cd8426a9fd36f53d021ab06332d821
ajay <ajayshakya7860@gmail.com>
2025-10-07 11:30:29 +0530

    conflict solving

--- src/services/axios/axios.type.tsx
+++ src/services/axios/axios.type.tsx
@@ -1,13 +1,4 @@
-import type React from "react";
-import type { AxiosInstance, AxiosRequestConfig } from "axios";
-
-export interface ServiceConfig {
-  name: string;
-  baseURL: string;
-  timeout?: number;
-  headers?: Record<string, string>;
-}
-
-export interface Axiostype {
-  axiosInstance: AxiosInstance;
-}
+import { AxiosInstance } from "axios";
+export type Axiostype = {
+  axios: AxiosInstance;
+};
